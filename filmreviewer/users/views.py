d063b59f932174201ccbed38da24eb1dceca74d6
danielfsantoss <danielfigueiredo009@gmail.com>
2025-03-01 23:17:35 -0300

    feat: views.py atualizado com os imports. mãos a obra

--- filmreviewer/users/views.py
+++ filmreviewer/users/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .api.serializer import UserSerializer
+
+import json
 
 # Create your views here.
 
