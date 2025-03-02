8fa9c9888eebb8f925868ccbf6d43f85629f149e
danielfsantoss <danielfigueiredo009@gmail.com>
2025-03-01 23:04:13 -0300

    refactor: app "user" substituido por "users"

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-
-patterns = [
-  path('criar/', 'user.views.criar'),
-  path('listar/', 'user.views.listar'),
-  path('editar/', 'user.views.editar'),
-  path('deletar/', 'user.views.deletar'),
-]
