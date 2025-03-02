31865b16c802650a46035c7c0e8802bf034c8c61
danielfsantoss <danielfigueiredo009@gmail.com>
2025-03-01 21:44:07 -0300

    feat: Models adicionados

--- filmreviewer/users/models.py
+++ filmreviewer/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.username
+
+
+class Reviews(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    review = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.review
+    
